90736be4247d44dbdf0fa98daedb2289a4907946
Denis Kovalev <kovvdn@gmail.com>
2022-10-14 20:25:46 +0300

    complete two tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,7 +14,38 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 function transform(arr) {
-  throw new NotImplementedError("Not implemented");
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const array = [...arr];
+  const result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const el = array[i];
+
+    switch (el) {
+      case "--double-next":
+        array[i + 1] && result.push(array[i + 1]);
+        break;
+      case "--discard-next":
+        array[i + 1] && array.splice(i + 1, 1, null);
+        break;
+
+      case "--double-prev":
+        array[i - 1] && result.push(result.at(-1));
+        break;
+
+      case "--discard-prev":
+        array[i - 1] && result.pop();
+        break;
+
+      default:
+        el && result.push(el);
+        break;
+    }
+  }
+  return result;
 }
 
 module.exports = {
