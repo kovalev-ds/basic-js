16dfc033b011b302da4a55c6391b374cf35c8f22
Denis Kovalev <kovvdn@gmail.com>
2022-10-15 00:47:21 +0300

    complete base-js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,84 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
+
+//  Encryption
+//  The plaintext(P) and key(K) are added modulo 26.
+//  Ei = (Pi + Ki) mod 26
+
+//  Decryption
+//  Di = (Ei - Ki + 26) mod 26
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  #alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  #isDirect;
+
+  constructor(isDirect = true) {
+    this.#isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    if (!str || !key) throw new Error("Incorrect arguments!");
+
+    str = str.toUpperCase();
+    key = "".padStart(str.length, key).toUpperCase();
+
+    const result = Array.from({ length: str.length }, (_, i) => {
+      const strIdx = this.#alphabet.indexOf(str[i]);
+
+      if (strIdx === -1) {
+        key = key.substring(0, i) + str[i] + key.substring(i);
+      }
+
+      const keyIdx = this.#alphabet.indexOf(key[i]);
+
+      return strIdx > -1 ? this.#alphabet[(strIdx + keyIdx) % 26] : str[i];
+    });
+
+    return this.#isDirect ? result.join("") : result.reverse().join("");
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) throw new Error("Incorrect arguments!");
+
+    str = str.toUpperCase();
+    key = "".padStart(str.length, key).toUpperCase();
+
+    const result = Array.from({ length: str.length }, (_, i) => {
+      const strIdx = this.#alphabet.indexOf(str[i]);
+
+      if (strIdx === -1) {
+        key = key.substring(0, i) + str[i] + key.substring(i);
+      }
+
+      const keyIdx = this.#alphabet.indexOf(key[i]);
+
+      return strIdx > -1 ? this.#alphabet[(strIdx - keyIdx + 26) % 26] : str[i];
+    });
+
+    return this.#isDirect ? result.join("") : result.reverse().join("");
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
